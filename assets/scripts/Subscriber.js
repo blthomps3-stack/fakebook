30da337a57b9db6eebba055f9656314e6933182f
Blake Thompson <blthomps3@gmail.com>
2026-03-26 12:56:15 -0500

    initial commit

---
+++ assets/scripts/Subscriber.js
@@ -0,0 +1,16 @@
+class Subscriber extends User {
+    #pages;
+    #groups;
+    #canMonetize;
+
+    constructor(id, name, userName, email, pages, groups, canMonetize){
+        super(id, name, userName, email);
+        this.pages = pages;
+        this.groups = groups;
+        this.canMonetize = canMonetize;
+    }
+
+    get pages() {return this.#pages;}
+    get groups() {return this.#groups;}
+    get canMonetize() {return this.#canMonetize;}
+}
